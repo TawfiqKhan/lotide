392ae58342bd6084359af864459ede2696c4bc7a
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-04-26 15:59:08 -0400

    assertion message styled and working

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+//Function Implementation
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`%cAssertion Passed: ${actual} === ${expected}`, `color: green`);
+  } else {
+    console.log(`%cAssertion Failed: ${actual} !== ${expected}`, `color: red`);
+  }
+};
+
+//Test Code
+
+assertEqual("Lighouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("1", 1);
+assertEqual(1, 5);
+
