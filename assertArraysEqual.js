108792c5e7adb62a9cb3bfac21b2f2f1643e4ac2
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-04-29 18:00:44 -0400

    comments added

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+// For TesTing/Assertion
+
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅✅ %cTest Passed: ${arr1} & ${arr2} are equal`, `color: green`);
