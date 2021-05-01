834482735cc3dd6b93441047f26e4e5aa0fba0f5
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-01 16:14:46 -0400

    recursion added for eqArrays, eqObjects

--- eqArrays.js
+++ eqArrays.js
@@ -2,19 +2,28 @@
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`%cAssertion Passed: ${actual} === ${expected}`, `color: green`);
+    console.log(`✅✅  %cAssertion Passed: ${actual} === ${expected}`, `color: green`);
   } else {
-    console.log(`%cAssertion Failed: ${actual} !== ${expected}`, `color: red`);
+    console.log(`⛔️⛔️ %cAssertion Failed: ${actual} !== ${expected}`, `color: red`);
   }
 };
 
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length)
+  if (arr1.length !== arr2.length) {
+    console.log("returning from line 14:  ");
     return false;
-
+  }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i])
-      return false;
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else {
+      if (arr1[i] !== arr2[i]) {
+        console.log("From line 24: ", "item 1:", arr1[i], "item 2: ", arr2[i]);
+        return false;
+      }
+    }
   }
   return true;
 };
@@ -22,4 +31,8 @@ const eqArrays = function(arr1, arr2) {
 //Test Code
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
-assertEqual(eqArrays([4, 2, 7], [4, 2, 7]), false);
+assertEqual(eqArrays([4, 2, 7], [4, 2, 7]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+assertEqual(eqArrays([[2, 2], [4]], [[2, 2], [4]]), true); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
--- eqObjects.js
+++ eqObjects.js
@@ -1,6 +1,6 @@
 //For Testing
 
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅ %cAssertion Passed: ${actual} === ${expected}`, `color: green`);
   } else {
@@ -10,36 +10,36 @@ const assertEqual = function(actual, expected) {
 
 //For comparing Arrays
 
-const eqArrays = function(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
+  console.log("arr1 L: ", arr1.length, "arr2 L: ", arr2.length);
   if (arr1.length !== arr2.length)
     return false;
-
   for (let i = 0; i < arr1.length; i++) {
+    console.log("We are Here...");
     if (arr1[i] !== arr2[i])
       return false;
   }
   return true;
 };
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-
-//First need to get the keys, theen use each keys to look for their value in both object.
-
 // Main Logic Function
 
-const eqObjects = function(object1, object2) {
+const eqObjects = function (object1, object2) {
+  if (typeof object1 !== "object" || typeof object2 !== "object")
+    return false;
   let object1Keys = Object.keys(object1);
   let object2Keys = Object.keys(object2);
   if (object1Keys.length !== object2Keys.length) {
     return false;
   }
   for (let item of object1Keys) {
-    if (!Array.isArray(object1[item]) || !Array.isArray(object2[item])) {
-      if (object1[item] !== object2[item]) {
+    if (Array.isArray(object1[item]) && Array.isArray(object2[item])) { // if not array
+      return eqArrays(object1[item], object2[item]);
+    } else if (typeof object1[item] === "object" && typeof object1[item] === "object") { // if Object
+      if (!object1[item], object2[item]) {
         return false;
       }
-    } else {
-      return eqArrays(object1[item], object2[item]);
+    } else if (object1[item] !== object2[item]) { // if item not object and if they dont match
+      return false;
     }
   }
   return true;
@@ -49,7 +49,6 @@ const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const ca = { b: "1", a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
-
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
@@ -59,3 +58,8 @@ assertEqual(eqObjects(cd, dc), true);
 assertEqual(eqObjects(cd, cd2), false);
 assertEqual(eqObjects(ab, ba), true);
 assertEqual(eqObjects(ab, abc), false);
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2, c: { z: 1 } }, { a: { z: 1 }, b: 2, c: { z: 2 } }), false);  // => true
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+
