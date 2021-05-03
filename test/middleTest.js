e3e420b316739b39ac03ef495df9f9840f558180
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-03 16:29:04 -0400

    mocha test for tailTest, middleTest Added

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,19 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle')
+const assert = require('chai').assert;
+const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2, 3]), [2]); // Should pass
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); //Should pass
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [4]); //Should fail
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); //Should pass
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#Middle", () => {
+  it("returns [2] from Array [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3],), [2]);
+  });
+
+  it("returns [3] from Array [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5],), [3]);
+  });
+
+  it("returns [3, 4] from Array [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6],), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,26 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const tail = require("../tail");
 
-const result = tail([1, 2, 3, 4, 5]);
-assertEqual(result.length, 3);
-assertEqual(result[0], 3);
-assertEqual(result[1], 3);
+const result = tail([1, 2, 3, 4]);
+// assertEqual(result.length, 3);
+// assertEqual(result[0], 3);
+// assertEqual(result[1], 3);
 
 const words = ["Yo Yo"];
-tail(words);
-assertEqual(words.length, 1); // original array should still have 3 elements!
+// tail(words);
+// assertEqual(words.length, 1); // original array should still have 3 elements!
+
+describe("#Tail", () => {
+  it("expect Length to be 3 for [1, 2, 3, 4] ", () => {
+    assert.strictEqual(result.length, 3)
+  });
+
+  it("returns tail [2, 3, 4] for [1, 2, 3, 4] ", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4])
+  });
+
+  it("original array should still have 3 elements!", () => {
+    tail(words);
+    assert.strictEqual(words.length, 1)
+  });
+})
